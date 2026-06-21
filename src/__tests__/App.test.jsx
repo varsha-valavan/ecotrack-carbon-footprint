73262549645041f8de5c3cc2d103e4779a8a607a
Varsha V <varshavalavan@gmail.com>
2026-06-21 11:16:46 +0530

    Added unit testing with Vitest

---
+++ src/__tests__/App.test.jsx
@@ -0,0 +1,8 @@
+import { test, expect } from "vitest";
+import { render } from "@testing-library/react";
+import App from "../App";
+
+test("App renders without crashing", () => {
+  const { container } = render(<App />);
+  expect(container).toBeTruthy();
+});
